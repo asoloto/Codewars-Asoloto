b2247bbb313bcf69e4aff79f9b7d2b040b82157d
asoloto <mailto:asoloto07@gmail.com>
2022-03-05 00:20:36 -0500

    ejercicio 1 completado

--- squareDigits.js
+++ squareDigits.js
@@ -1,8 +1,10 @@
 /*
 Welcome. In this kata, you are asked to square every digit of a number and concatenate them.
 
-For example, if we run 9119 through the function, 811181 will come out, because 92 is 81 and 12 is 1.
+For example, if we run 9119 through the function, 811181 will come out, because 9^2 is 81 and 1^2 is 1.
 */
 function squareDigits(num){
-    return 0;
+    return Number(String(num).split("").map(n=>(Math.pow(Number(n),2))).join(""));
 }
+
+console.log(squareDigits(9119));
